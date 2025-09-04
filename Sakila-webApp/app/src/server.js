14b8151490c525b2f2d959686ce01597c9b76330
S.k2004 <79660690+s-kroonen@users.noreply.github.com>
2025-09-04 10:46:06 +0200

    moved docker-compose

--- Sakila-webApp/app/src/server.js
+++ Sakila-webApp/app/src/server.js
@@ -5,5 +5,11 @@ const PORT = process.env.PORT || 3000;
 const server = http.createServer(app);
 server.listen(PORT, function () {
     // Callback, no promises
+    console.log({
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        pass: process.env.DB_PASSWORD ? 'set' : 'missing'
+    });
+
     console.log('[server] listening on port', PORT);
 });
